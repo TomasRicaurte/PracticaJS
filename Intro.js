74b8327fe7d3da23f9ba85ac5b26d957c558b7ca
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-09 22:08:52 -0500

    Primera practica de java

---
+++ Intro.js
@@ -0,0 +1,13 @@
+let variable = 3
+let string ="Nombre"
+let x ="4"
+let boolean = 15 / 5
+
+ 
+let array = [2, "texto", false, 3.5]
+
+console.log(variable)
+console.log(string)
+console.log(x)
+console.log(boolean)
+console.log(array.length)
