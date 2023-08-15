31b51472dadfbe6d10ae7b74f971c9e54845535d
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-15 13:29:07 -0500

    Practica String completa

---
+++ StringPractica.js
@@ -0,0 +1,30 @@
+let miRefran
+miRefran = "No hay mal que por bien no venga"
+
+let longitudRefran;
+longitudRefran = (miRefran.length)
+
+console.log(longitudRefran)
+
+let indice = 9;
+let novenaLetra;
+novenaLetra = miRefran.charAt(indice)
+
+console.log(novenaLetra)
+
+indice = 31;
+let ultimaLetra;
+ultimaLetra = miRefran.charAt(indice)
+
+console.log(ultimaLetra)
+
+let indicePalabra
+indicePalabra = miRefran.indexOf("que");
+
+console.log(indicePalabra)
+
+miRefran = "No hay mal (palabra robada) por bien no venga"
+
+let Mayusculas = miRefran.toUpperCase();
+
+console.log(Mayusculas)
