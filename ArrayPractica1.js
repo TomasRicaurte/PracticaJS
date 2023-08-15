214ca00e20bbcd31c08bcde5302c427aa04e603a
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-15 14:51:49 -0500

    Practica Array basica

--- ArrayPractica1.js
+++ ArrayPractica1.js
@@ -1,2 +1,16 @@
-let myArray = ["Monsters inc", "Toy story", "El extraño mundo de Jack"]
+let myArray = ["Monsters inc", "Toy story", "El extraño mundo de Jack"];
 
+console.log(myArray)
+
+myArray[0] = "Vacas vaqueras";
+myArray[1] = "Las cronicas de narnia";
+
+console.log(myArray);
+
+myArray.unshift("Ready Player One")
+
+console.log(myArray)
+
+let ElementosSeparados = myArray.join('-')
+
+console.log(ElementosSeparados)
