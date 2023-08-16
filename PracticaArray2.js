5af8b257f18206cb98605814ed00a1eb9fc7d522
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-15 20:32:30 -0500

    Practica 2 de array completa

--- PracticaArray2.js
+++ PracticaArray2.js
@@ -1,2 +1,17 @@
 let myString = "Javascript+es+super+cool" ;
+    console.log(myString)
 
+let myArray2 = myString.split('+')
+    console.log(myArray2)
+
+let ArrayLength = myArray2.length
+    console.log(ArrayLength)
+
+let LastItem =myArray2[ArrayLength - 1]
+    console.log(LastItem)
+
+let ArrayInverso = myArray2.slice().reverse()
+    console.log(ArrayInverso)
+
+ArrayInverso.push("el")
+    console.log(ArrayInverso)
